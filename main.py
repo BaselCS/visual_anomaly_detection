9a6434a342c2d8f68b66d918bf4ac221e4ec759d
BaselCS <baselxx1xx@gmail.com>
2026-02-28 21:31:50 +0300

    نتائج التدريب المعياري

--- main.py
+++ main.py
@@ -40,10 +40,10 @@ DEFAULT_CONFIG = {
     "image_size": 512,
     "train_split": 0.9,               # 90% training, 10% validation split 
     "seed": 999,
-    "early_stop_patience": 20,     # Stop if no improvement for x epochs
+    "early_stop_patience": 30,     # Stop if no improvement for x epochs
     "early_stop_min_delta": 1e-4,
-    "min_epochs_before_early_stop": 20,
-    "num_workers": 2,              # number of subprocesses for data loading, adjust based on CPU cores and memory, Windows users may want to set this to 0 for compatibility
+    "min_epochs_before_early_stop": 50, # Don't allow early stopping before this many epochs
+    "num_workers": 0,              # number of subprocesses for data loading, adjust based on CPU cores and memory, Windows users may want to set this to 0 for compatibility
 }
 
 # Configure logging
